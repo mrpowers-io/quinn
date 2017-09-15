c73d4529495c99a6ff59d425c7d6c5f837166df7
MrPowers <matthewkevinpowers@gmail.com>
2017-09-15 16:31:10 -0500

    Add a DataFrame transformation and a Spark test to the project

--- quinn/hello_world.py
+++ quinn/hello_world.py
@@ -3,3 +3,6 @@ class HelloWorld:
     def hello(self):
         return "greetings matthew"
 
+    def with_age_plus_two(self, df):
+        return df.withColumn('age_plus_two', df.age + 2)
+
---
+++ quinn/spark.py
@@ -0,0 +1,7 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+  .master("local") \
+  .appName("pyspark-spec") \
+  .getOrCreate()
+
