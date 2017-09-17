4e1959be6089ac8f28681bdb4d20cdee6bdbf5c9
MrPowers <matthewkevinpowers@gmail.com>
2017-09-17 15:31:39 -0500

    Make sure the validate_schema doesn't raise an error when the actual schema matches the required schema

--- tests/test_dataframe_validator.py
+++ tests/test_dataframe_validator.py
@@ -3,7 +3,7 @@ import pytest
 from quinn.spark import *
 from quinn.dataframe_validator import *
 
-from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.types import StructType, StructField, StringType, LongType
 
 class TestDataFrameValidator(object):
 
@@ -29,3 +29,12 @@ class TestDataFrameValidator(object):
         with pytest.raises(DataFrameMissingStructFieldError) as excinfo:
             DataFrameValidator().validate_schema(source_df, required_schema)
         assert excinfo.value.args[0] == "The [StructField(city,StringType,true)] StructFields are not included in the DataFrame with the following StructFields StructType(List(StructField(name,StringType,true),StructField(age,LongType,true)))"
+
+    def test_validate_schema_when_struct_field_is_missing(self):
+        data = [("jose", 1), ("li", 2), ("luisa", 3)]
+        source_df = spark.createDataFrame(data, ["name", "age"])
+        required_schema = StructType([
+            StructField("name", StringType(), True),
+            StructField("age", LongType(), True),
+        ])
+        DataFrameValidator().validate_schema(source_df, required_schema)
