19688215ccfdd4afd48662159f38908c4b8d5293
MrPowers <matthewkevinpowers@gmail.com>
2017-09-18 08:37:03 -0500

    Add sort_columns transformation to reorder the columns in a DataFrame in ascending or descending order

--- quinn/transformations.py
+++ quinn/transformations.py
@@ -1,5 +1,15 @@
 from functools import reduce
 
+def sort_columns(df, sort_order):
+    sorted_col_names = None
+    if sort_order == "asc":
+        sorted_col_names = sorted(df.columns)
+    elif sort_order == "desc":
+        sorted_col_names = sorted(df.columns, reverse=True)
+    else:
+        raise ValueError(f"['asc', 'desc'] are the only valid sort orders and you entered a sort order of '{sort_order}'")
+    return df.select(*sorted_col_names)
+
 def snake_case_col_names(df):
     return reduce(
         lambda memo_df, col_name: memo_df.withColumnRenamed(col_name, to_snake_case(col_name)),
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -31,3 +31,83 @@ class TestTransformations(object):
         )
 
         assert(expected_df.collect() == actual_df.collect())
+
+    def test_sort_columns_asc(self):
+        source_df = spark.createDF(
+            [
+                ("jose", "oak", "switch"),
+                ("li", "redwood", "xbox"),
+                ("luisa", "maple", "ps4"),
+            ],
+            [
+                ("name", StringType(), True),
+                ("tree", StringType(), True),
+                ("gaming_system", StringType(), True),
+            ]
+        )
+
+        actual_df = QT.sort_columns(source_df, "asc")
+
+        expected_df = spark.createDF(
+            [
+                ("switch", "jose", "oak"),
+                ("xbox", "li", "redwood"),
+                ("ps4", "luisa", "maple"),
+            ],
+            [
+                ("gaming_system", StringType(), True),
+                ("name", StringType(), True),
+                ("tree", StringType(), True),
+            ]
+        )
+
+        assert(expected_df.collect() == actual_df.collect())
+
+    def test_sort_columns_desc(self):
+        source_df = spark.createDF(
+            [
+                ("jose", "oak", "switch"),
+                ("li", "redwood", "xbox"),
+                ("luisa", "maple", "ps4"),
+            ],
+            [
+                ("name", StringType(), True),
+                ("tree", StringType(), True),
+                ("gaming_system", StringType(), True),
+            ]
+        )
+
+        actual_df = QT.sort_columns(source_df, "desc")
+
+        expected_df = spark.createDF(
+            [
+                ("oak", "jose", "switch"),
+                ("redwood", "li", "xbox"),
+                ("maple", "luisa", "ps4"),
+            ],
+            [
+                ("tree", StringType(), True),
+                ("name", StringType(), True),
+                ("gaming_system", StringType(), True),
+            ]
+        )
+
+        assert(expected_df.collect() == actual_df.collect())
+
+    def test_sort_columns_exception(self):
+        source_df = spark.createDF(
+            [
+                ("jose", "oak", "switch"),
+                ("li", "redwood", "xbox"),
+                ("luisa", "maple", "ps4"),
+            ],
+            [
+                ("name", StringType(), True),
+                ("tree", StringType(), True),
+                ("gaming_system", StringType(), True),
+            ]
+        )
+
+        with pytest.raises(ValueError) as excinfo:
+            actual_df = QT.sort_columns(source_df, "cats")
+        assert excinfo.value.args[0] == "['asc', 'desc'] are the only valid sort orders and you entered a sort order of 'cats'"
