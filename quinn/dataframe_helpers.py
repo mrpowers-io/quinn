353f46404eea27f67b2775b4823c6707d0ca3867
MrPowers <matthewkevinpowers@gmail.com>
2017-09-20 08:04:33 -0500

    Replace a map with a list comprehension

--- quinn/dataframe_helpers.py
+++ quinn/dataframe_helpers.py
@@ -1,7 +1,7 @@
 class DataFrameHelpers:
 
     def column_to_list(self, df, col_name):
-        return list(map(lambda r: getattr(r, col_name), df.collect()))
+        return [x[col_name] for x in df.select(col_name).collect()]
 
     def two_columns_to_dictionary(self, df, key_col_name, value_col_name):
         l = list(map(lambda r: (getattr(r, key_col_name), getattr(r, value_col_name)), df.collect()))
