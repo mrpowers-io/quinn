3d4645cac71aae4420a0c909d665739bb7beaa46
MrPowers <matthewkevinpowers@gmail.com>
2017-09-20 09:40:46 -0500

    Use a dictionary comprehension to be more Pythonic

--- quinn/dataframe_helpers.py
+++ quinn/dataframe_helpers.py
@@ -4,8 +4,8 @@ class DataFrameHelpers:
         return [x[col_name] for x in df.select(col_name).collect()]
 
     def two_columns_to_dictionary(self, df, key_col_name, value_col_name):
-        l = list(map(lambda r: (getattr(r, key_col_name), getattr(r, value_col_name)), df.collect()))
-        return dict(l)
+        k, v = key_col_name, value_col_name
+        return {x[k]: x[v] for x in df.select(k, v).collect()}
 
     def to_list_of_dictionaries(self, df):
         return list(map(lambda r: r.asDict(), df.collect()))
