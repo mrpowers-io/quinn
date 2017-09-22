d2a5d8dc2a57ff2bbb6cadc7295c6c9a9b67693c
MrPowers <matthewkevinpowers@gmail.com>
2017-09-21 21:09:50 -0500

    Bump to v0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests_require = ['pytest']
 
 setup(
     name='quinn',
-    version='0.0.1',
+    version='0.1.0',
     author='Matthew Powers',
     author_email='matthewkevinpowers@gmail.com',
     url='https://github.com/MrPowers/quinn',
