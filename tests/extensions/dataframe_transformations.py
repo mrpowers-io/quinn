421a0f0edb2cec96e15fd9a4c6b4bfb530d3e76a
MrPowers <matthewkevinpowers@gmail.com>
2017-11-01 12:10:04 -0500

    Fix whitespace per PEP8

--- tests/extensions/dataframe_transformations.py
+++ tests/extensions/dataframe_transformations.py
@@ -1,15 +1,19 @@
 from pyspark.sql.functions import lit
 
+
 def with_greeting(df):
     return df.withColumn("greeting", lit("hi"))
 
+
 def with_something(df, something):
     return df.withColumn("something", lit(something))
+
 
 def with_funny(word):
     def inner(df):
         return df.withColumn("funny", lit(word))
     return inner
+
 
 def with_jacket(word, df):
     return df.withColumn("jacket", lit(word))
