5a364e13f1a9520e6e31d7632e07d9168cca3a08
nathanlim <nathanlim45@gmail.com>
2018-01-30 17:37:33 -0500

    Add assert_column_equality function

---
+++ quinn/assertion_helpers.py
@@ -0,0 +1,19 @@
+class ColumnNotAssertError(ValueError):
+    """raise this when there's a DataFrame column error"""
+
+
+def assert_column_equality(df, col_name1, col_name2):
+    list1 = df.select(col_name1).collect()
+    list2 = df.select(col_name2).collect()
+
+    if(list1 == list2) :
+        return True
+
+    else :
+        error_message = "The values of {col_name1} column are different from {col_name2}".format(
+            col_name1 = col_name1,
+            col_name2 = col_name2
+        )
+        print(list1)
+        print(list2)
+        raise ColumnNotAssertError(error_message)
---
+++ tests/test_assertion_helpers.py
@@ -0,0 +1,38 @@
+import pytest
+
+from quinn.spark import *
+import quinn
+
+class TestAssertionHelpers:
+
+    def test_assert_column_equality_when_equal(self):
+        data = [("jose", 1, 1), ("li", 2, 2), ("luisa", 3, 3)]
+        source_df = spark.createDataFrame(data, ["name", "col1", "col2"])
+        quinn.assert_column_equality(source_df, "col1", "col2")
+
+    def test_assert_column_equality_when_different(self):
+        data = [("jose", 1, 1), ("li", 2, 2), ("luisa", 3, 5)]
+        source_df = spark.createDataFrame(data, ["name", "col1", "col2"])
+
+        with pytest.raises(quinn.ColumnNotAssertError) as excinfo:
+            quinn.assert_column_equality(source_df, "col1", "col2")
+        assert excinfo.value.args[0] == "The values of col1 column are different from col2"
+
+    def test_assert_column_equality_equal_with_none(self):
+        data = [("jose", None, None), ("li", 2, 2), ("luisa", 3, 3)]
+        source_df = spark.createDataFrame(data, ["name", "col1", "col2"])
+        quinn.assert_column_equality(source_df, "col1", "col2")
+
+    def test_assert_column_equality_not_equal_with_none(self):
+        data = [("jose", None, 0), ("li", 2, 2), ("luisa", 3, 3)]
+        source_df = spark.createDataFrame(data, ["name", "col1", "col2"])
+
+        with pytest.raises(quinn.ColumnNotAssertError) as excinfo:
+            quinn.assert_column_equality(source_df, "col1", "col2")
+        assert excinfo.value.args[0] == "The values of col1 column are different from col2"
+
+    def test_assert_column_equality_equal_with_text(self):
+        data = [("jose", "", ""), ("li", "student", "student"), ("luisa", "teacher", "teacher")]
+        source_df = spark.createDataFrame(data, ["name", "col1", "col2"])
+        quinn.assert_column_equality(source_df, "col1", "col2")
+
