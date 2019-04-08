f10250ace208ad8ba7ff6fbdab824b51d76bed5c
Albert Franzi <franzicros@gmail.com>
2019-04-08 16:48:32 +0200

    [QUINN-16] - Stop importing a default Spark Session (#17)

--- quinn/extensions/spark_session_ext.py
+++ quinn/extensions/spark_session_ext.py
@@ -1,12 +1,10 @@
-from quinn.spark import *
-
 from pyspark.sql.types import StructType, StructField
 from pyspark.sql import SparkSession
 
 
 def create_df(self, rows_data, col_specs):
     struct_fields = list(map(lambda x: StructField(*x), col_specs))
-    return spark.createDataFrame(rows_data, StructType(struct_fields))
+    return self.createDataFrame(data=rows_data, schema=StructType(struct_fields))
 
 
 SparkSession.create_df = create_df
