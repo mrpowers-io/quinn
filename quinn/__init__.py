de4420f06290148d14b5201b1fac3d6399482d4e
Sem Sinchenko <semyonsinchenko@yahoo.com>
2023-03-09 12:13:36 +0100

    Refactoring
    - move `print_schema_as_code` into `schema_helpers.py`
    - modify `__init__.py`
    - drop `DataFrame.printSchemaAsCode`
    - re-organize tests
    
    Final solution does not modify `DataFrame` object but create a separate helper
    function instead.
    
     On branch schema-pretty-print
     Changes to be committed:
    	modified:   quinn/__init__.py
    	modified:   quinn/extensions/dataframe_ext.py
    	new file:   quinn/schema_helpers.py
    	modified:   tests/extensions/test_dataframe_ext.py
    	new file:   tests/test_schema_helpers.py

--- quinn/__init__.py
+++ quinn/__init__.py
@@ -1,5 +1,6 @@
 from .dataframe_helpers import *
 from .dataframe_validator import *
+from .schema_helpers import print_schema_as_code
 from .functions import *
 from .scala_to_pyspark import ScalaToPyspark
 from .transformations import *
