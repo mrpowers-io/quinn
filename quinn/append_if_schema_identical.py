63cbd0d7f3cdd1623b020f2349e6294e1e3a280c
Puneet Sharma <65413434+puneetsharma04@users.noreply.github.com>
2023-04-02 20:25:33 +0200

    Made the changes as per the review comments

--- quinn/append_if_schema_identical.py
+++ quinn/append_if_schema_identical.py
@@ -6,7 +6,15 @@ class SchemaMismatchError(ValueError):
     """raise this when there's a schema mismatch between source & target schema"""
 
 
-def append_if_schema_identical(source_df: DataFrame, target_df: DataFrame):
+def append_if_schema_identical(source_df: DataFrame, target_df: DataFrame) -> DataFrame:
+    """Compares the schema of source & target dataframe .
+    :param source_df: Input DataFrame
+    :type source_df: pyspark.sql.DataFrame
+    :param target_df: Input DataFrame
+    :type target_df: pyspark.sql.DataFrame
+    :return: dataframe
+    :rtype: pyspark.sql.DataFrame
+    """
     # Retrieve the schemas of the source and target dataframes
     source_schema = source_df.schema
     target_schema = target_df.schema
@@ -16,18 +24,12 @@ def append_if_schema_identical(source_df: DataFrame, target_df: DataFrame):
     target_schema_list = [(field.name, str(field.dataType)) for field in target_schema]
 
     unmatched_cols = [col for col in source_schema_list if col not in target_schema_list]
-
-    print(source_schema_list)
-    print(type(source_schema_list[0][0]))
-    print(type(target_schema_list[0][0]))
-    print(target_schema_list)
-    error_message = f"The schemas of the source and target dataframes are not identical.From source schema column {unmatched_cols} is missing in target schema"
-    expected_schema = target_schema_list
-    # Compare the two schema lists
-    if source_schema_list == target_schema_list:
-        # Append the dataframes if the schemas are identical
-        appended_df = source_df.union(target_df)
-        return appended_df
-    else:
-        # Raise an error if the schemas are different
+    error_message = f"The schemas of the source and target dataframes are not identical." \
+                    f"From source schema column {unmatched_cols} is missing in target schema"
+    if set(source_schema.fieldNames()) != set(target_schema.fieldNames()):
         raise SchemaMismatchError(error_message)
+    if sorted(source_schema_list) != sorted(target_schema_list):
+        raise SchemaMismatchError(error_message)
+
+    appended_df = target_df.unionByName(source_df)
+    return appended_df
