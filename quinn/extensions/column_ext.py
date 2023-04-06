7e17c4d361caeb7a53336dcb7305f3048241c522
Sem Sinchenko <semyonsinchenko@yahoo.com>
2023-04-06 13:58:21 +0200

    Docstrings to public methods and functions.
    Created with OpenAI davinchi-text-003 and validated/fixed by myself.
    
     On branch feature/90-docstrings_public_functions
     Changes to be committed:
     - modified:   quinn/dataframe_helpers.py
     - modified:   quinn/dataframe_validator.py
     - modified:   quinn/extensions/column_ext.py
     - modified:   quinn/extensions/spark_session_ext.py
     - modified:   quinn/functions.py
     - modified:   quinn/transformations.py

--- quinn/extensions/column_ext.py
+++ quinn/extensions/column_ext.py
@@ -5,22 +5,66 @@ from pyspark.sql.functions import lit, trim, when
 
 
 def isFalsy(self: Column) -> Column:
+    """Returns a Column indicating whether all values in the Column are False or NULL
+    (**falsy**). Each element in the resulting column is True if all the elements in the
+    Column are either NULL or False, or False otherwise. This is accomplished by
+    performing a bitwise or of the ``isNull`` condition and a literal False value and
+    then wrapping the result in a **when** statement.
+
+    :param self: Column object
+    :returns: Column object
+    :rtype: Column
+    """
     return when(self.isNull() | (self == lit(False)), True).otherwise(False)
 
 
 def isTruthy(self: Column) -> Column:
+    """Calculates a boolean expression that is the opposite of isFalsy for the given
+    ``Column`` self.
+
+    :param Column self: The ``Column`` to calculate the opposite of isFalsy for.
+    :returns: A ``Column`` with the results of the calculation.
+    :rtype: Column
+    """
     return ~(self.isFalsy())
 
 
 def isFalse(self: Column) -> Column:
+    """This function checks if the column is equal to False and returns the column.
+
+    :param self: Column
+    :return: Column
+    :rtype: Column
+    """
     return self == False
 
 
 def isTrue(self: Column) -> Column:
+    """
+    This function takes a column of type Column as an argument and returns a column
+    of type Column.
+
+    It evaluates whether each element in the column argument is equal to True, and
+    if so will return True, otherwise False.
+
+    :param self: Column object
+    :returns: Column object
+    :rtype: Column
+    """
     return self == True
 
 
 def isNullOrBlank(self: Column) -> Column:
+    """Returns a Boolean value which expresses whether a given column is ``null`` or
+    contains only blank characters.
+
+    :param \*\*self: The  :class:`Column` to check.
+
+    :returns: A `Column` containing ``True`` if the column is ``null`` or only contains
+    blank characters, or ``False`` otherwise.
+    :rtype: Column
+    """
+
     return (self.isNull()) | (trim(self) == "")
 
 
--- quinn/transformations.py
+++ quinn/transformations.py
@@ -5,6 +5,19 @@ from pyspark.sql import DataFrame
 
 
 def with_columns_renamed(fun: Callable[[str], str]) -> Callable[[DataFrame], DataFrame]:
+    """This is a function designed to rename the columns of a
+    `Spark DataFrame`.
+
+    It takes a `Callable[[str], str]` object as an argument (``fun``) and returns a
+    `Callable[[DataFrame], DataFrame]` object.
+
+    When `_()` is called on a `DataFrame`, it creates a list of column names,
+    applying the argument `fun()` to each of them, and returning a new `DataFrame`
+    with the new column names.
+
+    :param fun: Renaming function
+    :returns: Function which takes DataFrame as parameter.
+    """
     def _(df: DataFrame) -> DataFrame:
         cols = list(
             map(
@@ -20,6 +33,20 @@ def with_columns_renamed(fun: Callable[[str], str]) -> Callable[[DataFrame], Dat
 def with_some_columns_renamed(
     fun: Callable[[str], str], change_col_name: Callable[[str], str]
 ) -> Callable[[DataFrame], DataFrame]:
+    """A function that takes a `Callable[[str], str]` and a `Callable[[str], str]`
+    and returns a `Callable[[DataFrame], DataFrame]`, which in turn takes a
+    `DataFrame` and returns a `DataFrame` with some of its columns renamed.
+
+    :param fun: A function that takes a column name as a string and returns a
+    new name as a string.
+    :type fun: `Callable[[str], str]`
+    :param change_col_name: A function that takes a column name as a string and
+    returns a boolean.
+    :type change_col_name: `Callable[[str], str]`
+    :return: A `Callable[[DataFrame], DataFrame]`, which takes a
+    `DataFrame` and returns a `DataFrame` with some of its columns renamed.
+    :rtype: `Callable[[DataFrame], DataFrame]`
+    """
     def _(df):
         cols = list(
             map(
@@ -35,14 +62,42 @@ def with_some_columns_renamed(
 
 
 def snake_case_col_names(df: DataFrame) -> DataFrame:
+    """This function takes a ``DataFrame`` instance and returns the
+    same ``DataFrame`` instance with all column names converted to snake case
+    (e.g. ``col_name_1``). It uses the ``to_snake_case`` function in conjunction with
+    the ``with_columns_renamed`` function to achieve this.
+    :param df: A ``DataFrame`` instance to process
+    :type df: ``DataFrame``
+    :return: A ``DataFrame`` instance with column names converted to snake case
+    :rtype: ``DataFrame``
+    """
     return with_columns_renamed(to_snake_case)(df)
 
 
 def to_snake_case(s: str) -> str:
+    """Takes a string and converts it to snake case format.
+
+    :param s: The string to be converted.
+    :type s: str
+    :return: The string in snake case format.
+    :rtype: str
+    """
     return s.lower().replace(" ", "_")
 
 
 def sort_columns(df: DataFrame, sort_order: str) -> DataFrame:
+    """This function sorts the columns of a given DataFrame based on a given sort
+    order. The ``sort_order`` parameter can either be ``asc`` or ``desc``, which correspond to
+    ascending and descending order, respectively. If any other value is provided for
+    the ``sort_order`` parameter, a ``ValueError`` will be raised.
+
+    :param df: A DataFrame
+    :type df: pandas.DataFrame
+    :param sort_order: The order in which to sort the columns in the DataFrame
+    :type sort_order: str
+    :return: A DataFrame with the columns sorted in the chosen order 
+    :rtype: pandas.DataFrame
+    """
     sorted_col_names = None
     if sort_order == "asc":
         sorted_col_names = sorted(df.columns)
