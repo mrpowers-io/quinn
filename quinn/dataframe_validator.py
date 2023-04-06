e1d0a840910de22bca4e16d09ee13cc6fddb97d7
Semyon <semyonsinchenko@yahoo.com>
2023-04-06 23:03:18 +0200

    Docstrings to public methods and functions. (#91)
    Created with OpenAI davinchi-text-003 and validated/fixed by myself.
    
     On branch feature/90-docstrings_public_functions
     Changes to be committed:
     - modified:   quinn/dataframe_helpers.py
     - modified:   quinn/dataframe_validator.py
     - modified:   quinn/extensions/column_ext.py
     - modified:   quinn/extensions/spark_session_ext.py
     - modified:   quinn/functions.py
     - modified:   quinn/transformations.py

--- quinn/dataframe_validator.py
+++ quinn/dataframe_validator.py
@@ -18,6 +18,16 @@ class DataFrameProhibitedColumnError(ValueError):
 
 
 def validate_presence_of_columns(df: DataFrame, required_col_names: List[str]) -> None:
+    """Validates the presence of column names in a DataFrame.
+
+    :param df: A spark DataFrame.
+    :type df: DataFrame`
+    :param required_col_names: List of the required column names for the DataFrame.
+    :type required_col_names: :py:class:`list` of :py:class:`str`
+    :return: None.
+    :raises DataFrameMissingColumnError: if any of the requested column names are
+    not present in the DataFrame.
+    """
     all_col_names = df.columns
     missing_col_names = [x for x in required_col_names if x not in all_col_names]
     error_message = "The {missing_col_names} columns are not included in the DataFrame with the following columns {all_col_names}".format(
@@ -28,6 +38,21 @@ def validate_presence_of_columns(df: DataFrame, required_col_names: List[str]) -
 
 
 def validate_schema(df: DataFrame, required_schema: StructType, ignore_nullable: bool=False) -> None:
+    """
+    This function will validate that a given DataFrame has a given StructType as its
+    schema.
+
+    :param df: DataFrame to validate
+    :type df: DataFrame
+    :param required_schema: StructType required for the DataFrame
+    :type required_schema: StructType
+    :param ignore_nullable: (Optional) A flag for if nullable fields should be
+    ignored during validation
+    :type ignore_nullable: bool, optional
+
+    :raises DataFrameMissingStructFieldError: if any StructFields from the required
+    schema are not included in the DataFrame schema
+    """
     _all_struct_fields = copy.deepcopy(df.schema)
     _required_schema = copy.deepcopy(required_schema)
 
@@ -48,6 +73,15 @@ def validate_schema(df: DataFrame, required_schema: StructType, ignore_nullable:
 
 
 def validate_absence_of_columns(df: DataFrame, prohibited_col_names: List[str]) -> None:
+    """
+    Validate that none of the prohibited column names are present among
+    specified DataFrame columns.
+
+    :param df: DataFrame containing columns to be checked.
+    :param prohibited_col_names: List of prohibited column names.
+    :raises DataFrameProhibitedColumnError: If the prohibited column names are
+    present among the specified DataFrame columns.
+    """
     all_col_names = df.columns
     extra_col_names = [x for x in all_col_names if x in prohibited_col_names]
     error_message = "The {extra_col_names} columns are not allowed to be included in the DataFrame with the following columns {all_col_names}".format(
