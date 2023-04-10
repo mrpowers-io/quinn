09b13219790258fe196188d3fef004955b9d071b
Puneet Sharma <65413434+puneetsharma04@users.noreply.github.com>
2023-04-11 00:20:50 +0200

    Made changes to include split_col function.

--- quinn/__init__.py
+++ quinn/__init__.py
@@ -6,3 +6,4 @@ from .functions import *
 from .scala_to_pyspark import ScalaToPyspark
 from .transformations import *
 from .append_if_schema_identical import append_if_schema_identical
+from .split_columns import split_col
