141355e1b20cd74ec755ddc0bffca1e048f19bd1
dinjazelena <132609540+dinjazelena@users.noreply.github.com>
2023-10-07 18:47:32 +0200

    Allow-all-ruff-rules (#135)
    * apply ruff on schema_helpers
    
    * apply ruff to spark module
    
    * apply ruff on transformations module
    
    * add extend ignore
    
    * fix badge'
    
    * apply ruff to spark extension
    
    * fix extensions

--- quinn/extensions/__init__.py
+++ quinn/extensions/__init__.py
@@ -1,3 +1,5 @@
-from .column_ext import *  # noqa
-from .dataframe_ext import *  # noqa
-from .spark_session_ext import *  # noqa
+"""Extensions API."""
+
+from quinn.extensions.column_ext import *
+from quinn.extensions.dataframe_ext import *
+from quinn.extensions.spark_session_ext import *
--- quinn/extensions/dataframe_ext.py
+++ quinn/extensions/dataframe_ext.py
@@ -1,8 +1,10 @@
 import warnings
+
 from pyspark.sql.dataframe import DataFrame
+from typing_extensions import Self
 
 
-def _ext_function(self, f):
+def _ext_function(self: Self, f: object) -> object:
     warnings.warn(
         "Extensions may be removed in the future versions of quinn. Please use explicit functions instead",
         category=DeprecationWarning,
