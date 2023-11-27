cf65fb892184eb19fe7fb64f58fbdf3536fe0164
SemyonSinchenko <semyonsinchenko@yahoo.com>
2023-11-27 09:36:37 +0100

    Move an API to experimental
     On branch feature/plan-utils
     Changes to be committed:
    	new file:   quinn/experimental/__init__.py
    	renamed:    quinn/plan_utils.py -> quinn/experimental/plan_utils.py

---
+++ quinn/experimental/__init__.py
@@ -0,0 +1,7 @@
+"""Experimental API.
+
+These APIs are unstable because they use non-stable parts of PySpark API.
+"""
+
+from quinn.experimental.plan_utils import estimate_size_of_df  # noqa: F401, I001
+from quinn.experimental.plan_utils import get_plan_from_df  # noqa: F401
