a938d6a05205409293cdea458b6cb14c3bfac9ab
jeffbrennan <jeffbrennan10@gmail.com>
2023-11-28 11:13:46 -0500

    calculate runs needed based on first run time

--- benchmarks/benchmark_column_performance.py
+++ benchmarks/benchmark_column_performance.py
@@ -5,21 +5,37 @@ import timeit
 from pathlib import Path
 
 
-def auto_timeit(stmt: str = "pass", setup: str = "pass") -> list[float]:
+def auto_timeit(
+    stmt: str = "pass",
+    setup: str = "pass",
+    min_runtime_seconds: int = 2,
+) -> list[float]:
     """Automatically determine the number of runs to perform to get a minimum."""
-    min_run_time_seconds = 2
-    runtime_multiplier = 10
-    n = 1
-    t = timeit.repeat(stmt, setup, repeat=n, number=1)
+    min_runs = 5
+    print(f"Running {stmt} 1 time...")
+    t = timeit.repeat(stmt, setup, repeat=1, number=1)
 
-    while sum(t) < min_run_time_seconds:
-        n *= runtime_multiplier
-        t = timeit.repeat(stmt, setup, repeat=n, number=1)
+    print(f"First run: {t[0]:.2f} seconds")
+    if t[0] >= min_runtime_seconds:
+        return t
 
-    return t
+    expected_runs_needed = int((min_runtime_seconds // t[0]) + 1)
+    if expected_runs_needed < min_runs:
+        expected_runs_needed = min_runs
+
+    expected_runtime = t[0] * expected_runs_needed
+    print(
+        f"Running {stmt} {expected_runs_needed} times. Expected runtime: {expected_runtime:.2f} seconds..."
+    )
+    return timeit.repeat(stmt, setup, repeat=expected_runs_needed, number=1)
 
 
-def get_result(test_name: str, dataset: dict, expr: str) -> None:
+def get_result(
+    test_name: str,
+    dataset: dict,
+    expr: str,
+    min_runtime_seconds: int,
+) -> None:
     """Run a test and save the results to a file."""
     setup = f"""import timeit
 import pyspark.sql.functions as F
@@ -34,7 +50,7 @@ spark = builder.getOrCreate()
 {dataset['name']} = spark.read.parquet('benchmarks/data/mvv_{dataset['name']}')
 """
     stmt = expr.replace("df", dataset["name"])
-    result = auto_timeit(stmt, setup)
+    result = auto_timeit(stmt, setup, min_runtime_seconds)
 
     summary = {
         "test_name": test_name,
@@ -57,10 +73,10 @@ config = {
 
 
 DATASETS = {
-    "xsmall": {"name": "xsmall", "size": 1_000},
-    "small": {"name": "small", "size": 100_000},
-    "medium": {"name": "medium", "size": 10_000_000},
-    "large": {"name": "large", "size": 100_000_000},
+    "large": {"name": "large", "size": 100_000_000, "min_runtime_seconds": 1200},
+    "medium": {"name": "medium", "size": 10_000_000, "min_runtime_seconds": 360},
+    "small": {"name": "small", "size": 100_000, "min_runtime_seconds": 20},
+    "xsmall": {"name": "xsmall", "size": 1_000, "min_runtime_seconds": 20},
 }
 
 for test_name, test_config in config.items():
@@ -68,4 +84,9 @@ for test_name, test_config in config.items():
     for dataset_name in DATASETS:
         dataset = DATASETS[dataset_name]
         print(f"TESTING DATASET {dataset['name']} [n={dataset['size']:,}]")
-        get_result(test_name=test_name, dataset=dataset, expr=test_config["expr"])
+        get_result(
+            test_name=test_name,
+            dataset=dataset,
+            expr=test_config["expr"],
+            min_runtime_seconds=dataset["min_runtime_seconds"],
+        )
