4b97a75f6466ab941de347765a4b1f9d3ec7741f
jeffbrennan <jeffbrennan10@gmail.com>
2023-12-20 19:09:46 -0500

    update figures, organize into functions for use in doc notebook

--- benchmarks/visualize_benchmarks.py
+++ benchmarks/visualize_benchmarks.py
@@ -40,7 +40,7 @@ def parse_results(spark: SparkSession) -> tuple[pd.DataFrame]:
 
     average_df = (
         result_df[["test_name", "dataset_size", "runtime"]]
-        .groupby(["test_name", "dataset_size"])
+        .groupby(["test_name", "dataset_size"], observed=False)
         .mean()
         .reset_index()
     )
@@ -50,6 +50,10 @@ def parse_results(spark: SparkSession) -> tuple[pd.DataFrame]:
 
 def show_boxplot(df: pd.DataFrame) -> None:
     """Displays faceted boxplot of benchmark results."""
+    today_str = pd.Timestamp.today().strftime("%Y-%m-%d")
+    machine_config = "Python 3.12.0, Spark 3.5, Pandas 2.1.3, M1 Macbook Pro 32GB RAM"
+    subtitle = f"<sup>{today_str} | {machine_config}</sup>"
+
     fig = px.box(
         df,
         x="dataset_size_formatted",
@@ -57,40 +61,48 @@ def show_boxplot(df: pd.DataFrame) -> None:
         color="test_name",
         facet_col="dataset_name",
         points="all",
-        title="Benchmark Results<br><sup>Spark 3.5, Python 3.12, M1 Macbook Pro 32GB RAM</sup></br>",
+        title=f"Column to List Benchmark Results<br>{subtitle}</br>",
         labels={"runtime": "Runtime (seconds)"},
         category_orders={"dataset_name": ["xsmall", "small", "medium", "large"]},
     )
     fig.update_yaxes(matches=None)
     fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
     fig.update_xaxes(matches=None, title=None)
+    fig.update_layout(legend_title_text="")
+
     fig.show()
 
 
 def show_line_plot(df: pd.DataFrame) -> None:
     """Displays line plot of average benchmark results."""
+    today_str = pd.Timestamp.today().strftime("%Y-%m-%d")
+    machine_config = "Python 3.12.0, Spark 3.5, Pandas 2.1.3, M1 Macbook Pro 32GB RAM"
+    subtitle = f"<sup>{today_str} | {machine_config}</sup>"
     fig = px.line(
         df,
         x="dataset_size",
         y="runtime",
         log_x=True,
         color="test_name",
-        title="Benchmark Results<br><sup>Spark 3.5, Python 3.12, M1 Macbook Pro 32GB RAM</sup></br>",
-        labels={"runtime": "Runtime (seconds)"},
+        title=f"Column to List Benchmark Results<br>{subtitle}</br>",
+        labels={"runtime": "Runtime (seconds)", "dataset_size": "Number of Rows"},
     )
     fig.update_traces(mode="markers+lines")
     fig.update_traces(marker={"size": 15})
+    fig.update_layout(legend_title_text="")
+
     fig.show()
 
 
-spark = (
-    SparkSession.builder.appName("MyApp")
-    .config("spark.executor.memory", "10G")
-    .config("spark.driver.memory", "25G")
-    .config("spark.sql.shuffle.partitions", "2")
-    .getOrCreate()
-)
+if __name__ == "__main__":
+    spark = (
+        SparkSession.builder.appName("MyApp")
+        .config("spark.executor.memory", "10G")
+        .config("spark.driver.memory", "25G")
+        .config("spark.sql.shuffle.partitions", "2")
+        .getOrCreate()
+    )
 
-result_df, average_df = parse_results(spark)
-show_boxplot(result_df)
-show_line_plot(average_df)
+    result_df, average_df = parse_results(spark)
+    show_boxplot(result_df)
+    show_line_plot(average_df)
