f6040848e35615bc7c4293c15af7b2f1b5dbdb45
jeffbrennan <jeffbrennan10@gmail.com>
2024-02-24 15:30:01 -0500

    ruff lint

--- quinn/keyword_finder.py
+++ quinn/keyword_finder.py
@@ -1,18 +1,19 @@
+from __future__ import annotations
+
 import os
 from glob import iglob
 
-
 default_keywords = [
     "_jsc",
-    "_jconf", 
-    "_jvm", 
-    "_jsparkSession", 
-    "_jreader", 
-    "_jc", 
-    "_jseq", 
-    "_jdf", 
-    "_jmap", 
-    "_jco"
+    "_jconf",
+    "_jvm",
+    "_jsparkSession",
+    "_jreader",
+    "_jc",
+    "_jseq",
+    "_jdf",
+    "_jmap",
+    "_jco",
     "emptyRDD",
     "range",
     "init_batched_serializer",
@@ -40,38 +41,80 @@ default_keywords = [
 ]
 
 
-def search_file(path, keywords=default_keywords):
+def search_file(path: str, keywords: list[str] = default_keywords) -> None:
+    """Searches a file for keywords and prints the line number and line containing the keyword.
+
+    :param path: The path to the file to search.
+    :type path: str
+    :param keywords: The list of keywords to search for.
+    :type keywords: list[str]
+    :returns: None
+    :rtype: None
+
+    """
     print(f"\nSearching: {path}")
     with open(path) as f:
         for line_number, line in enumerate(f, 1):
-            for keyword in keywords:    
+            for keyword in keywords:
                 if keyword in line:
-                    print(f"{line_number}: {keyword_format(line)}", end='')
+                    print(f"{line_number}: {keyword_format(line)}", end="")
                     break
 
 
-def search_files(path, keywords=default_keywords):
+def search_files(path: str, keywords: list[str] = default_keywords) -> None:
+    """Searches all files in a directory for keywords.
+
+    :param path: The path to the directory to search.
+    :type path: str
+    :param keywords: The list of keywords to search for.
+    :type keywords: list[str]
+    :returns: None
+    :rtype: None
+
+    """
     rootdir_glob = f"{path}/**/*"
     file_list = [f for f in iglob(rootdir_glob, recursive=True) if os.path.isfile(f)]
     for f in file_list:
-        search_file(f)
+        search_file(f, keywords)
 
 
-def keyword_format(input, keywords=default_keywords):
-    nc = '\033[0m'
-    red = '\033[31m'
-    bold = '\033[1m'
+def keyword_format(input: str, keywords: list[str] = default_keywords) -> str:
+    """Formats the input string to highlight the keywords.
+
+    :param input: The string to format.
+    :type input: str
+    :param keywords: The list of keywords to highlight.
+    :type keywords: list[str]
+
+    """
+    nc = "\033[0m"
+    red = "\033[31m"
+    bold = "\033[1m"
     res = input
     for keyword in keywords:
-        res = surround_substring(res, keyword, red+bold, nc)
+        res = surround_substring(res, keyword, red + bold, nc)
     return res
 
 
-def surround_substring(input, substring, surround_start, surround_end):
+def surround_substring(input: str, substring: str, surround_start: str, surround_end: str) -> str:
+    """Surrounds a substring with the given start and end strings.
+
+    :param input: The string to search.
+    :type input: str
+    :param substring: The substring to surround.
+    :type substring: str
+    :param surround_start: The string to start the surrounding with.
+    :type surround_start: str
+    :param surround_end: The string to end the surrounding with.
+    :type surround_end: str
+    :returns: The input string with the substring surrounded.
+    :rtype: str
+
+    """
     index = input.find(substring)
     res = ""
     if index == -1:
         res = input
     else:
-        res = input[:index] + surround_start + substring + surround_end + input[(index+len(substring)):]
+        res = input[:index] + surround_start + substring + surround_end + input[(index + len(substring)) :]
     return res
--- tests/test_keyword_finder.py
+++ tests/test_keyword_finder.py
@@ -1,17 +1,20 @@
-import pytest
 from quinn.keyword_finder import search_file, search_files, keyword_format, surround_substring
+
 
 def test_search_file():
     search_file("tests/test_files/some_pyspark.py")
 
+
 def test_search_files():
     search_files("tests/test_files")
+
 
 def test_keyword_format():
     print(keyword_format("spark rdd stuff"))
     print(keyword_format("spark rdd stuff with bad _jvm"))
     print(keyword_format("nice string"))
     print(keyword_format(""))
+
 
 def test_surround_substring():
     print(surround_substring("spark rdd stuff", "rdd", "**", "||"))
