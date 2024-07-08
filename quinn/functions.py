0823158f3516cd49c8698997cbec9efe10f19f9f
Niju Vijayakumar <nijanthanvijayakumar@outlook.com>
2024-07-08 11:57:50 +1000

    Remove imported and unused Callable module to avoid ruff lint failure

--- quinn/functions.py
+++ quinn/functions.py
@@ -16,7 +16,6 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
     from numbers import Number
 
     from pyspark.sql import Column
