0c6422aae3202bb609c4e4a8f43efb680c242824
Niju Vijayakumar <nijanthanvijayakumar@outlook.com>
2024-08-06 16:52:48 +1000

    Include the doc-string for the Spark-Connect wrapper function

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -20,6 +20,19 @@ from functools import wraps
 
 
 def check_spark_connect_compatibility(func):
+    """
+    Decorator to check Spark-Connect compatibility for a given function.
+
+    This decorator will check the Spark version and the environment variable
+    `SPARK_CONNECT_MODE_ENABLED`. If the Spark version is less than 3.5.2 and
+    `SPARK_CONNECT_MODE_ENABLED` is set, it will raise an exception when the
+    decorated function is called. Otherwise, it will execute the function normally.
+
+    :param func: The function to be decorated.
+    :type func: function
+    :return: The wrapped function with Spark-Connect compatibility check.
+    :rtype: function
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         spark_version = spark.version
